b774df99fd46f8222c05b55367ec97f449ced165
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-03-07 00:41:13 +0200

    create navigator to add_bottom

--- nootee_app/lib/views/widget/edit_note_view.dart
+++ nootee_app/lib/views/widget/edit_note_view.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import 'edit_view_body.dart';
+
 class edit_note_view extends StatelessWidget {
   const edit_note_view({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: edit_note_view_body(),
-    )
+      body: edit_view_body(),
+    ),
   }
 }
---
+++ nootee_app/lib/views/widget/edit_view_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class edit_view_body extends StatelessWidget {
+  const edit_view_body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
