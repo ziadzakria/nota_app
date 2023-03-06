a3a3455f7a92d279e74e879a4cb4bf6d080fb354
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-03-06 17:12:33 +0200

    create Text_Field

---
+++ nootee_app/lib/views/widget/custom_text_field.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class custom_text_field extends StatelessWidget {
+  const custom_text_field({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
