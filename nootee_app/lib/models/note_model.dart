6c7da2bc058de90c636647eca1afd1d8bbe29a8a
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-03-07 23:14:43 +0200

    create_part_note_model.g.dart

--- nootee_app/lib/models/note_model.dart
+++ nootee_app/lib/models/note_model.dart
@@ -1,11 +1,27 @@
-class note_model {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveTypt(TypeId: 0)
+class note_model extends HiveObject {
+  @HiveTypt(TypeId: 0)
   final String title;
+  @HiveTypt(TypeId: 1)
   late final String subtitle;
+  @HiveTypt(TypeId: 2)
   final String date;
+  @HiveTypt(TypeId: 3)
   final int color;
+  @HiveTypt(TypeId: 4)
   note_model(
       {required this.title,
       required this.subtitle,
       required this.date,
       required this.color});
 }
+
+class HiveTypt {
+  const HiveTypt({required int TypeId});
+}
+
+class HiveObject {}
