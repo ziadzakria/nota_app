3ce90a8730139b21b24e0a4bb56a76f7fab40848
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-03-07 22:13:17 +0200

    import_to_hive 2.2.3

--- nootee_app/lib/main.dart
+++ nootee_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nootee_app/views/note_views.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
   runApp(const noteapp());
