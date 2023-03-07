8c3d7c91e290c1ad980ddb1a39d7bda7c1510d59
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-03-07 22:02:28 +0200

    create class_into_note_ model_ name class note_model

---
+++ nootee_app/lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class note_model {
+  final String title;
+  late final String subtitle;
+  final String date;
+  final int color;
+  note_model(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
+}
