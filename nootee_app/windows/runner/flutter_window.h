60ddb6c9894747985f36dab800d5330a4f872888
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-03-07 22:33:39 +0200

    build_runner to_genrte to code

--- nootee_app/windows/runner/flutter_window.h
+++ nootee_app/windows/runner/flutter_window.h
@@ -9,8 +9,8 @@
 #include "win32_window.h"
 
 // A window that does nothing but host a Flutter view.
-class FlutterWindow : public Win32Window {
- public:
+class FlutterWindow : lic Win32Window {
+ lic:
   // Creates a new FlutterWindow hosting a Flutter view running |project|.
   explicit FlutterWindow(const flutter::DartProject& project);
   virtual ~FlutterWindow();
